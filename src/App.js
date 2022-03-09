6dec459a6ac9ebb92e154d6ecdf5d78a189fba06
parth-bhadra <63583662+parth-bhadra@users.noreply.github.com>
2022-03-09 19:33:26 +0530

    segway to useContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { useState,  useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-    
-    if(storedUserLoggedInInformation === '1'){
+
+    if (storedUserLoggedInInformation === '1') {
       setIsLoggedIn(true);
     }
   }, []) // no dependencies mean, this runs only when the component is mounted and never after that, which is what we want
@@ -26,15 +27,19 @@ function App() {
     localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
-
+// without the value attribute it will crash,
+// in auth-context.js if you provide a deafult value and do not want it to change then you should not use provider at all and everything will work fine
+// but if your value changes and you want that state change to be propogated everywhere this prop is needed in the app, then pass in the state variable
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
