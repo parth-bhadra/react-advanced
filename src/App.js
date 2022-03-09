5d393a8c94eda5b4a136de0c70c5fcc47a654200
parth-bhadra <63583662+parth-bhadra@users.noreply.github.com>
2022-03-09 20:26:53 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,40 +6,15 @@ import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []) // no dependencies mean, this runs only when the component is mounted and never after that, which is what we want
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
-// without the value attribute it will crash,
-// in auth-context.js if you provide a deafult value and do not want it to change then you should not use provider at all and everything will work fine
-// but if your value changes and you want that state change to be propogated everywhere this prop is needed in the app, then pass in the state variable
+  const authCtx = useContext(AuthContext);
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn
-    }}>
-      <MainHeader onLogout={logoutHandler} />
+    <React.Fragment>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!authCtx.isLoggedIn && <Login />}
+        {authCtx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => { }, // this is done for better ide auto-completion, nothing else
+    onLogin: (email, password) => { }
 })
 // AuthContext contains a component
+
+// taking code out of App.js and refactoring
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    
+        if (storedUserLoggedInInformation === '1') {
+          setIsLoggedIn(true);
+        }
+      }, []) // no dependencies mean, this runs only when the component is mounted and never after that, which is what we want
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    }
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    }
+    
+
+    return <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+    }}>
+        {props.children}
+    </AuthContext.Provider>
+}
 
 export default AuthContext;
